83f0d77e3a5b8be6854b88076ac9a34417026ac4
eliran moyal <eliran.moyal1@gmail.com>
2012-12-30 13:13:07 +0200

    added layout links and integ tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,25 +1,25 @@
-require 'spec_helper'
+  require 'spec_helper'
 
-describe PagesController do
- render_views
- 
+  describe PagesController do
+   render_views
+   
 
- before(:each) do
-  @baseTitle = 'Ruby Sample App'
- end
+   before(:each) do
+    @baseTitle = 'Ruby Sample App'
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
-    
+
     it "should have good title" do
       get 'home'
       response.should have_selector("title", :content => "#{@baseTitle} | Home")
     end
-    
-      it "should not have blank body" do
+
+    it "should not have blank body" do
       get 'home'
       response.body.should_not =~ /<body>\s*<\/body>/
     end
@@ -30,21 +30,34 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
-     it "should have good title" do
+    it "should have good title" do
       get 'contact'
       response.should have_selector("title", :content => "#{@baseTitle} | Contact")
     end
   end
-  
-    describe "GET 'about'" do
+
+  describe "GET 'about'" do
     it "returns http success" do
       get 'about'
       response.should be_success
     end
-    
-     it "should have good title" do
+
+    it "should have good title" do
       get 'about'
       response.should have_selector("title", :content => "#{@baseTitle} | About")
+    end
+  end
+
+
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have good title" do
+      get 'help'
+      response.should have_selector("title", :content => "#{@baseTitle} | Help")
     end
   end
 
