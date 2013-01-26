885cfa55121ec270d2a8931f2d2c16e4cefce9f4
eliran moyal <eliran.moyal1@gmail.com>
2013-01-26 16:59:04 +0200

    added edit and update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 		if user.nil?
 			flash.now[:error] = "invalid password / email combination"
 			@title = "Sign in"
-			render 'new'		
+			render 'new'
 		else
 			sign_in(user)
 			redirect_to user_path(User.find_by_email(params[:session][:email]))
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,25 @@ class UsersController < ApplicationController
   	@user = User.new params[:user]
   	if @user.save
       sign_in(@user)
-  		redirect_to user_path(@user) , :flash => {:success => " #{@user.name} , welcome to our app !"}
-  	else
-  		@title = "Sign up"
-  		render 'new'
-  	end
+      redirect_to user_path(@user) , :flash => {:success => " #{@user.name} , welcome to our app !"}
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to user_path(@user) , :flash => {:success => "Update succeded"}
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
   end
 end
