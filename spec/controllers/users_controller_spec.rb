10c142dfeb551e0e0212e0a1889b9485a2664f81
eliran moyal <eliran.moyal1@gmail.com>
2013-02-01 00:42:57 +0200

    finished index page pagination

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,6 +38,14 @@ describe UsersController do
 					response.should have_selector('li',:content => user.name)
 				end
 			end
+
+			it "should paginate users" do
+				get :index
+				response.should have_selector('div.pagination')
+				response.should have_selector('span.disabled', :content => "Previous")
+				response.should have_selector('a',:href => "/users?page=2" , :content =>"2")
+				response.should have_selector('a',:href => "/users?page=2" , :content =>"Next")
+			end
 		end
 	end
 	describe "GET 'show'" do
